4609a311a6c511422ff6d2820840579f71ec96a7
ABresting <abhimanyur2010@gmail.com>
2023-01-05 12:00:28 +0100

    <your_commit_message>

--- app/server.go
+++ app/server.go
@@ -16,11 +16,25 @@ func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	if _,err :=conn.Read(buf); err != nil {
+		fmt.Println("error reading from client: ", err.Error())
+		os.Exit(1)
+	}
+
+	// wr have hardcoded the reply for now
+	conn.Write([]byte("+PONG\r\n"))
+
 }
